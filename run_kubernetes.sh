7cb28ce60601c31daebe78378684b118630112b8
achinnachot <achinnachot@ats-MacPro.local>
2019-08-12 01:16:20 -0700

    Completed task 7

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=atchinnachot/devops:ml-prediction1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run kube-prediction-app --image="${dockerpath}"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/kube-prediction-app 8000:80
